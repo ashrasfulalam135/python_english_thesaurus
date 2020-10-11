9b7353acebdd30190ce8c1e6f2983696166c652b
MD. Ashraful Alam <ashrafulalam135@gmail.com>
2020-10-11 23:02:07 +0900

    v1-finish

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # python_english_thesaurus
+
+#### Open gitbash and make directory
+```sh
+mkdir Desktop/python_projects/ 
+export DEV_HOME=Desktop/python_projects
+cd ${DEV_HOME}
+```
+
+#### Get the code from git
+```sh
+git clone https://github.com/ashrasfulalam135/python_english_thesaurus.git
+```
+
+#### Build image
+```sh
+export DEV_HOME=~/Desktop/python_projects/python_english_thesaurus/
+cd ${DEV_HOME}/docker_env
+docker-compose up -d
+```
+
+#### Run app
+```sh
+python3 app.py
+```
---
+++ english_thesaurus/app.py
@@ -0,0 +1,30 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(input_data):
+    converted_input_data = input_data.casefold()
+    if converted_input_data in data:
+        return data[converted_input_data]
+    elif len(get_close_matches(converted_input_data, data.keys(), cutoff=0.8)) > 0:
+        yn = input("Did you mean %s instead? Press 'Y' if yes, or 'N' if no: " % get_close_matches(converted_input_data, data.keys(), cutoff=0.8)[0])
+        if yn.casefold() == "y":
+            return data[get_close_matches(converted_input_data, data.keys(), cutoff=0.8)[0]]
+        elif yn.casefold() == "n":
+            return "%s is not exists. Please recheck." % input_data
+        else:
+            return "You didn't enter the right key. Try again"
+    else:
+        return "%s is not exists. Please recheck." % input_data
+
+word = input("Enter word: ")
+
+output = translate(word)
+
+if type(output) == list:
+    for defination in output:
+        print("- %s" % defination)
+else:
+    print(output)
+
